67eef03d76480ce80e827bf3c9ff15bdfbe6e469
Jeniffer Carvalho <jenicarvalho@live.com>
2022-01-12 15:27:41 -0300

    created App component and tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
+import SavingGoal from './pages/SavingGoal';
+
+import { globalStyle as GlobalStyle } from './styles/global';
 
 export function App(): JSX.Element {
-  return <div data-testid="greetings-container">Welcome to the Origin THA</div>;
+  return (
+    <div>
+      <Header />
+      <SavingGoal />
+      <GlobalStyle />
+    </div>
+  );
 }
