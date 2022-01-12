0b2c9bd461840d915e17415090b1bd6ca7288106
Jeniffer Carvalho <jenicarvalho@live.com>
2022-01-12 15:31:41 -0300

    added return type on function

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,4 +1,4 @@
-export const formatCurrency = (value: number) => {
+export const formatCurrency = (value: number): string => {
   return (value / 100).toLocaleString('en-US', {
     style: 'currency',
     currency: 'USD',
